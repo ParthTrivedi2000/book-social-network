938ee05f3cd787ef34b266aed6d47b8890326dc0
Parth Trivedi <trivediparth20000@gmail.com>
2025-01-24 23:31:47 +0530

    Importing value annotation from package

--- book-network-backend/src/main/java/com/champ/book/config/BeansConfig.java
+++ book-network-backend/src/main/java/com/champ/book/config/BeansConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Arrays;
 import java.util.Collections;
